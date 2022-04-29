5549c7975f8360bb60c2d16de9f5349d19a1b0b2
lprudhvi <104012723+lprudhvi@users.noreply.github.com>
2022-04-29 14:29:17 -0500

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -12,5 +12,5 @@ def deploy(ip,appname)
 }
 def test(jobname)
 {
-  sh 'java -jar /var/lib/jenkins/workspace/"${jobname}"/testing.jar'
+  sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
 }
